4ccc452d1a3570be1d186ef704b17b5b207f00a4
vantablanta <njerimwanjiku@gmail.com>
2021-07-16 12:43:34 +0300

    Task

---
+++ src/components/Task.vue
@@ -0,0 +1,25 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+         <SingleTask 
+            @toggle-reminder="$emit('toggle-reminder', task.id)"
+            :task="task" 
+            @delete-task="$emit('delete-task', task.id)"
+        />
+    </div>
+</template>
+
+<script>
+    import SingleTask from './SingleTask.vue'
+
+    export default {
+        name: 'Task',
+        components: {
+            SingleTask
+        },
+        emits: [
+            'delete-task','toggle-reminder'],
+        props: {
+            tasks: Array
+        }
+    }
+</script>
